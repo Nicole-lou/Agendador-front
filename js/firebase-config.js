1ed7cf45a12f19ffbb2bd5881e889e345c847e27
Nicole Lourenço <nicolelourenco38@gmail.com>
2026-04-29 17:51:19 -0300

    adicionando gitignore e config

---
+++ js/firebase-config.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAiDBqIcARavtSTl2hi96lVIp4sSwxp310",
+    authDomain: "agendador-horarios-trein-98de1.firebaseapp.com",
+    projectId: "agendador-horarios-trein-98de1",
+    storageBucket: "agendador-horarios-trein-98de1.firebasestorage.app",
+    messagingSenderId: "660506661401",
+    appId: "1:660506661401:web:01669b8def20d289d4d088"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
