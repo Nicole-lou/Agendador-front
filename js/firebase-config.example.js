9510a1bf0aab5567c551bb4c6943adc8f1f33943
Nicole Lourenço <nicolelourenco38@gmail.com>
2026-05-07 13:01:34 -0300

    Update firebase-config.example.js

--- js/firebase-config.example.js
+++ js/firebase-config.example.js
@@ -1,3 +1,11 @@
+/**
+ * Configuração: configuração do Projeto Firebase.
+ * Altere essas configurações conforme seu projeto no Firebase.com
+ *  - Entre no console do projeto no Firebase
+ *  - Clique na engrenagem
+ *  - Copie os dados do projeto nas chaves abaixo
+ */
+
 export const firebaseConfig = {
   apiKey: "SUA_CHAVE_AQUI",
   // ... restando dos campos vazios
