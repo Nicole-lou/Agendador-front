b96cab5198fd2be5b31191f5b6ad378e6292e299
Nicole Lourenço <nicolelourenco38@gmail.com>
2026-05-08 21:41:05 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,2 +1,5 @@
 // autenticação do login JS - Firebase
-
+document.querySelector('#google-login').addEventListener('click', function() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+});
