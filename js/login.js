9fc926a7f1e31aae77b77d4c4fadf5b4b6c02973
Nicole Lourenço <nicolelourenco38@gmail.com>
2026-06-07 19:42:59 -0300

    JS cadastro

--- js/login.js
+++ js/login.js
@@ -4,7 +4,8 @@
 import { 
     signInWithPopup, 
     GoogleAuthProvider, 
-    signInWithEmailAndPassword 
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 
 // Importamos a configuração que vem do outro arquivo
@@ -37,10 +38,75 @@ document.querySelector('#google-login').addEventListener('click', () => {
 
 // autenticação do login JS email e senha - Firebase
 
-createUserWithEmailAndPassword(auth, email, senha)
+const btnCriarConta = document.querySelector('.btn-cadastro');
+// Valores dos inputs na hora do clique de cadastro
+btnCriarConta.addEventListener('click', () => {
+    const name = document.querySelector('#register-name').value;
+    const email = document.querySelector('#register-email').value;
+    const senha = document.querySelector('#register-password').value;
+    const confirmPassword = document.querySelector('#register-confirm-password').value;
+
+    // Validação simples: verificar se as senhas coincidem
+    if (password !== confirmPassword) {
+        alert("As senhas devem ser iguais!");
+        return; // Para a execução se as senhas não coincidirem
+    }
+    // Validação de caracteres de senha
+    if(password.length < 6) {
+        alert("A senha deve conter no mínimo 6 caracteres!");
+        return; // Para a execução se a senha for muito curta
+    }
+
+    // chamada do firebase para criar a conta, usando os valores dos inputs
+    createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-        alert("Conta criada com sucesso! Bem-vinda!");
-    })
-    .catch((error) => {
-        console.error("Erro ao cadastrar:", error.code);
-    });
+    const user = userCredential.user;
+    console.log("Conta criada com sucesso:", user.uid);
+    alert(`Conta criada com sucesso! Bem-vinda, ${name}!`);
+
+    document.querySelector('#register-name').value = '';
+    document.querySelector('#register-email').value = '';
+    document.querySelector('#register-password').value = '';
+    document.querySelector('#register-confirm-password').value = '';
+})
+
+.catch((error) => {
+    console.error("Erro ao cadastrar:", error.code);
+});
+
+// Tratamento de erros específicos para melhorar a experiência do usuário
+if(error.code === 'auth/email-already-in-use') {
+    alert("Este e-mail já está em uso.")
+    } else if (error.code === 'auth/invalid-email') {
+        alert("O e-mail fornecido é inválido.");
+    } else {
+        alert("Erro ao criar a conta: " + error.message);
+    }
+});
+
+// validação para mostrar ou esconder a senha no formulário de cadastro
+const checkLoginPass = document.querySelector('#show-login-pass');
+const inputLoginPass = document.querySelector('#register-password');
+
+checkLoginPass.addEventListener('change', () => {
+    inputLogin.Pas.type = checkLoginPass.checked ? 'text' : 'password';
+});
+
+const checkRegisterPass = document.querySelector('#show-register-pass');
+const inputRegisterPass = document.querySelector('#register-password');
+const inputRegisterConfirm = document.querySelector('#register-confirm-password');
+
+checkRegisterPass.addEventListener('change', () => {
+    const tipo = checkRegisterPass.checked ? 'text' : 'password';
+    inputRegisterPass.type = tipo;
+    inputRegisterConfirm.type = tipo;
+});
+
+
+
+
+
+
+
+
+
