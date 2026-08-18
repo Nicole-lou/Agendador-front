62d559abfd46ca639b3d7fcfda6d74b2a48b8bb1
Nicole Lourenço <nicolelourenco38@gmail.com>
2026-08-17 22:19:39 -0300

    implementando react

---
+++ agendador-front-react/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container py-5 text-center">
+      <h1 className="gold-text fw-bold">Agenda Dourada</h1>
+      <p className="lead text-muted">Facilite sua vida com a nossa Agenda Dourada</p>
+    </div>
+  )
+}
+
+export default App
